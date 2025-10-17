8aeff2f4aea52941cc63d8e95628d5dceab103d9
teacherjobportal1 <teacherjobportal1@gmail.com>
2025-10-17 17:42:20 +0530

    Update data.js

--- JS/data.js
+++ JS/data.js
@@ -8,27 +8,7 @@ const schools = [
     whatsapp: "9876543210",
     email: "XYZ@gmail.com",
     findus: "https://maps.app.goo.gl/k4cgpw3WgsHKErjK9"
-  },
-  {
-    name: "XYZ Public School",
-    district: "Mahendergarh",
-    vacancies: "No Vacancies",
-    location: "Ward No. 5, Near Bus Stand, Mahendergarh",
-    contact: "9876543210",
-    whatsapp: "9876543210",
-    email: "XYZ@gmail.com",
-    findus: "https://maps.app.goo.gl/k4cgpw3WgsHKErjK9"
-  },
-    {
-    name: "kjfkjdabkv Public School",
-    district: "Mahendergarh",
-    vacancies: "No Vacancies",
-    location: "Ward No. 5, safdv, Mahendergarh",
-    contact: "9876543210",
-    whatsapp: "9876543210",
-    email: "ASSDDSGse@gmail.com",
-    findus: "https://maps.app.goo.gl/k4cgpw3WgsHKErjK9"
-  },
+  }
 
 
 ];
